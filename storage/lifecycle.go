95df584c3c73a784fa933853f729d3ea8e9da386
Martin Hutchinson <mhutchinson@gmail.com>
2024-12-18 15:51:25 +0000

    more docs

--- storage/lifecycle.go
+++ storage/lifecycle.go
@@ -33,13 +33,26 @@ type Driver struct {
 
 // LogReader provides read-only access to the log.
 type LogReader struct {
-	// TODO(mhutchinson): Write out the full docs for this here
+	// ReadCheckpoint returns the latest checkpoint available.
 	ReadCheckpoint func(ctx context.Context) ([]byte, error)
 
-	// TODO(mhutchinson): Write out the full docs for this here
+	// ReadTile returns the raw marshalled tile at the given coordinates, if it exists.
+	// The expected usage for this method is to derive the parameters from a tree size
+	// that has been committed to by a checkpoint returned by this log. Whenever such a
+	// tree size is used, this method will behave as per the https://c2sp.org/tlog-tiles
+	// spec for the /tile/ path.
+	//
+	// If callers pass in parameters that are not implied by a published tree size, then
+	// implementations _may_ act differently from one another, but all will act in ways
+	// that are allowed by the spec. For example, if the only published tree size has been
+	// for size 2, then asking for a partial tile of 1 may lead to some implementations
+	// returning not found, some may return a tile with 1 leaf, and some may return a tile
+	// with more leaves.
 	ReadTile func(ctx context.Context, level, index uint64, p uint8) ([]byte, error)
 
-	// TODO(mhutchinson): Write out the full docs for this here
+	// ReadEntryBundle returns the raw marshalled leaf bundle at the given coordinates, if
+	// it exists.
+	// The expected usage and corresponding behaviours are similar to ReadTile.
 	ReadEntryBundle func(ctx context.Context, index uint64, p uint8) ([]byte, error)
 }
 
@@ -48,7 +61,16 @@ type Appender struct {
 	LogReader
 
 	// Add adds a new entry to be sequenced.
-	// TODO(mhutchinson): Write out the full docs for this here
+	// This method quickly returns an IndexFuture, which will return the index assigned
+	// to the new leaf. Until this is returned, the leaf is not durably added to the log,
+	// and terminating the process may lead to this leaf being lost.
+	// Once the future resolves and returns an index, the leaf is durably sequenced and will
+	// be preserved even in the process terminates.
+	//
+	// Once a leaf is sequenced, it will be integrated into the tree soon (generally single digit
+	// seconds). Until it is integrated, clients of the log will not be able to verifiably access
+	// this value. Personalities that require blocking until the leaf is integrated can use the
+	// tessera.IntegrationAwaiter to wrap the call to this method.
 	Add func(ctx context.Context, entry *tessera.Entry) tessera.IndexFuture
 }
 
